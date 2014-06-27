f4e387353bee730d8f1cd21881983eff2c129ca7
sugilog <sugilog@gmail.com>
2014-06-27 23:26:03 +0900

    writer process test

--- lib/active_record_handlersocket/writer.rb
+++ lib/active_record_handlersocket/writer.rb
@@ -5,50 +5,60 @@ module ActiveRecordHandlerSocket
 
       hs_open_index index_key, :write
 
-      attributes = attributes.stringify_keys
-      setting    = hs_fetch_key index_key
-      id         = setting[:id]
+      setting = hs_fetch_key index_key
+      id      = setting[:id]
 
-      values = setting[:fields].inject [] do |map, field|
-        case value = attributes[field]
-        when nil
-          if /^(?:updated|created)_(?:at|on)$/ =~ field
-            value = hs_current_time_from_proper_timezone
-          end
-        when TrueClass, FalseClass
-          value = value ? 1 : 0
-        end
-
-        map.push value
-      end
-
-      signal, result = hs_write_connection.execute_insert id, values
-      result = result.first
-
-      case
-      when signal == 0
-        # return PRIMARY KEY value
-        result.first.to_i
-      when signal > 0
-        message =
-          case result.to_i
-          when 121
-            "duplicate entry"
-          else
-            "unknown: #{result}"
-          end
-
-        raise ArgumentError, "invalid argument given: #{message}"
-      else
-        hs_reset_opened_indexes
-        raise CannotConnectError, result
-      end
+      values = hs_to_a_write_values attributes, setting[:fields]
+      result = hs_write_connection.execute_insert id, values
+      hs_write_result result
     end
 
     module PrivateMethods
       def hs_current_time_from_proper_timezone
         time = default_timezone == :utc ? Time.now.utc : Time.now
         time.to_s(:db)
+      end
+
+      def hs_to_a_write_values(attributes, fields)
+        attributes = attributes.stringify_keys
+
+        fields.inject [] do |map, field|
+          case value = attributes[field]
+          when nil
+            if /^(?:updated|created)_(?:at|on)$/ =~ field
+              value = hs_current_time_from_proper_timezone
+            end
+          when TrueClass, FalseClass
+            value = value ? 1 : 0
+          when Time, Date
+            value = value.to_s(:db)
+          end
+
+          map.push value
+        end
+      end
+
+      def hs_write_result(result_on_single)
+        signal, result = result_on_single
+
+        case
+        when signal == 0
+          # return PRIMARY KEY value
+          result.first.first.to_i
+        when signal > 0
+          message =
+            case result.to_i
+            when 121
+              "duplicate entry"
+            else
+              "unknown: #{result}"
+            end
+
+          raise ArgumentError, "invalid argument given: #{message}"
+        else
+          hs_reset_opened_indexes
+          raise CannotConnectError, result
+        end
       end
     end
   end
--- spec/cases/writer_spec.rb
+++ spec/cases/writer_spec.rb
@@ -17,7 +17,7 @@ describe "WriterSpec" do
     klass.__send__ :hs_writer
   end
 
-  describe "#hscreate" do
+  describe "hscreate" do
     before :each do
       klass.delete_all
       another_klass.delete_all
@@ -76,9 +76,10 @@ describe "WriterSpec" do
         expect(record.updated_at).to be_within(2).of(Time.now)
       end
     end
+
   end
 
-  describe "#hscreate many" do
+  describe "hscreate many" do
     it "should available and countup with auto increment" do
       id = nil
 
@@ -95,6 +96,181 @@ AND    TABLE_NAME = '#{klass.table_name}'
 
       expect(id).to eql(klass.last.id)
       expect(id).to eql(auto_increment - 1 + 10000)
+    end
+  end
+
+  describe "hs_current_time_from_proper_timezone" do
+    before :each do
+      @default = klass.default_timezone
+    end
+
+    after :each do
+      klass.default_timezone = @default
+    end
+
+    it "should return utc time" do
+      klass.default_timezone = :utc
+
+      expect(klass.__send__(:hs_current_time_from_proper_timezone)).to eql(Time.now.utc.to_s(:db))
+    end
+
+    it "should return local time" do
+      klass.default_timezone = :local
+
+      expect(klass.__send__(:hs_current_time_from_proper_timezone)).to eql(Time.now.to_s(:db))
+    end
+  end
+
+  describe "hs_to_a_write_values" do
+    it "should attributes to array" do
+      values = klass.__send__ :hs_to_a_write_values, {:A => 1, :B => "b"}, ["A", "B"]
+      expect(values).to eql([1, "b"])
+
+      values = klass.__send__ :hs_to_a_write_values, {:A => 1, :B => "b"}, ["B", "A"]
+      expect(values).to eql(["b", 1])
+    end
+
+    context "when boolean given" do
+      it "should convert true to 1" do
+        values = klass.__send__ :hs_to_a_write_values, {:C => true}, ["C"]
+        expect(values).to eql([1])
+      end
+
+      it "should convert false to 0" do
+        values = klass.__send__ :hs_to_a_write_values, {:C => false}, ["C"]
+        expect(values).to eql([0])
+      end
+    end
+
+    context "when nil given" do
+      it "should not convert" do
+        values = klass.__send__ :hs_to_a_write_values, {:D => nil}, ["D"]
+        expect(values).to eql([nil])
+      end
+    end
+
+    context "when time given" do
+      it "should convert db format string" do
+        t = Time.now
+        values = klass.__send__ :hs_to_a_write_values, {:E => t}, ["E"]
+        expect(values).to eql([t.to_s(:db)])
+      end
+    end
+
+    context "when date given" do
+      it "should convert db format string" do
+        d = Date.today
+        values = klass.__send__ :hs_to_a_write_values, {:F => d}, ["F"]
+        expect(values).to eql([d.to_s(:db)])
+      end
+    end
+
+    context "when timestamp field is nil" do
+      it "should add timestamp" do
+        t = Time.now
+        values = klass.__send__ :hs_to_a_write_values, {}, ["updated_at", "created_at"]
+        expect(values).to eql([t.to_s(:db), t.to_s(:db)])
+
+        values = klass.__send__ :hs_to_a_write_values, {}, ["updated_on", "created_on"]
+        expect(values).to eql([t.to_s(:db), t.to_s(:db)])
+      end
+
+      it "should not fill for exist field" do
+        c = Time.now - 1
+        t = Time.now
+        expect(c.to_s(:db)).not_to eql(t.to_s(:db))
+
+        values = klass.__send__ :hs_to_a_write_values, {:created_at => c}, ["updated_at", "created_at"]
+        expect(values).to eql([t.to_s(:db), c.to_s(:db)])
+
+        values = klass.__send__ :hs_to_a_write_values, {:created_on => c}, ["updated_on", "created_on"]
+        expect(values).to eql([t.to_s(:db), c.to_s(:db)])
+      end
+    end
+
+    context "when less fields" do
+      it "should fit to given fields" do
+        values = klass.__send__ :hs_to_a_write_values, {:X => 1, :Y => 2, :Z => 3}, ["X", "Z"]
+        expect(values).to eql([1, 3])
+      end
+    end
+
+    context "when more fields" do
+      it "should fit to given fields" do
+        values = klass.__send__ :hs_to_a_write_values, {:X => 1, :Y => 2, :Z => 3}, ["W", "X", "Y", "Z"]
+        expect(values).to eql([nil, 1, 2, 3])
+      end
+    end
+  end
+
+  describe "hs_write_result" do
+    it "should return result number" do
+      number = klass.__send__ :hs_write_result, [0, [["10"]]]
+      expect(number).to eql(10)
+    end
+
+    context "when error signal 121 given" do
+      it "should raise error for duplicate entry" do
+        expect{
+          klass.__send__ :hs_write_result, [1, "121"]
+        }.to raise_error(ArgumentError)
+      end
+
+      it "should raise error has message for duplicate entry" do
+        message = nil
+
+        begin
+          klass.__send__ :hs_write_result, [1, "121"]
+        rescue ArgumentError => e
+          message = e.message
+        end
+
+        expect(message).to match(/duplicate entry/)
+      end
+    end
+
+    context "when another error given" do
+      it "should raise error for duplicate entry" do
+        expect{
+          klass.__send__ :hs_write_result, [1, "stmtnum"]
+        }.to raise_error(ArgumentError)
+      end
+    end
+
+    context "when connection error" do
+      before :each do
+        ActiveRecord::Base.hs_reconnect!
+
+        klass.hsfind_by_id 1
+        klass.hsfind_by_age_and_status 36, 0
+
+        another_klass.hsfind_by_id 1
+
+        klass.hscreate :name => "Test", :age => 24, :status => true
+
+        another_klass.hscreate :person_id => 1, :title => "Test"
+      end
+
+      after :each do
+        ActiveRecord::Base.hs_reconnect!
+      end
+
+      it "should raise error for connection loat" do
+        expect{
+          klass.__send__ :hs_write_result, [-1, "write: closed"]
+        }.to raise_error(ActiveRecordHandlerSocket::CannotConnectError)
+      end
+
+      it "should all indexes closed" do
+        expect( ActiveRecord::Base.__send__(:hs_indexes).map{|k, setting| setting[:opened] }.any? ).to be
+
+        begin
+          klass.__send__ :hs_write_result, [-1, "write: closed"]
+        rescue
+        end
+
+        expect( ActiveRecord::Base.__send__(:hs_indexes).map{|k, setting| !setting[:opened] }.all? ).to be
+      end
     end
   end
 end
