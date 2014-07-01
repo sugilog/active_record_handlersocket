c0f2496634e19d7b146b21cf7a5e915f58fe2c11
sugilog <sugilog@gmail.com>
2014-07-01 20:22:17 +0900

    interface add ( no test )

--- lib/active_record_handlersocket/active_record_handler_socket.rb
+++ lib/active_record_handlersocket/active_record_handler_socket.rb
@@ -4,14 +4,15 @@ module ActiveRecordHandlerSocket
   class UnknownIndexError  < StandardError; end
 
   def self.included(c)
-    c.extend Interface
+    c.extend ClassMethods
+    c.__send__ :include, InstanceMethods
 
     connection = Connection.establish_connection c.logger
     c.__send__ :cattr_accessor, :hs_connection
     c.hs_connection = connection
   end
 
-  module Interface
+  module ClassMethods
     def hs_reader(key, index, options = {})
       hs_connection.add_index_setting self, key, index, options
     end
@@ -39,6 +40,67 @@ module ActiveRecordHandlerSocket
 
     def hscreate(attributes)
       hs_connection.insert self, attributes
+    end
+
+    # no test
+    def hsupdate(id, attributes)
+      hs_connection.update self, id, attributes
+    end
+
+    # no test
+    def hsdelete(id)
+    end
+  end
+
+  # no test
+  module InstanceMethods
+    def hssave(validation = true)
+      case
+      when validation && !valid?
+        false
+      when new_record?
+        hscreate
+      else
+        hsupdate
+      end
+    end
+
+    def hscreate
+      case
+      when false == run_callback(:before_create)
+        false
+      when result = self.class.hscreate(self.attributes)
+        self.id = result
+        self.instance_variable_set :@new_record, false
+        run_callback :after_create
+        !!id
+      else
+        false
+      end
+    end
+
+    def hsupdate
+      case
+      when false == run_callback(:before_update)
+        callback_result
+      when self.class.hsupdate(self.id, self.attributes)
+        run_callback :after_update
+        true
+      else
+        false
+      end
+    end
+
+    def hsdestroy
+      case
+      when false == run_callback(:before_destroy)
+        callback_result
+      when self.class.hsdelete(self.id)
+        run_callback :after_destroy
+        true
+      else
+        false
+      end
     end
   end
 end
